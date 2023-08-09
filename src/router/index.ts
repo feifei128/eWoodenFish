9eb2cc499aa96e31c7444bd231d13c25cdcd68ff
feifei128 <545060094@qq.com>
2023-08-09 22:05:48 +0800

    fix: 部署后路由丢失问题

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,8 +6,8 @@ import {
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: () => import("../pages/WoodenFish.vue")
+    name: "/",
+    component: () => import("../pages/index.vue")
   },
 ];
 
